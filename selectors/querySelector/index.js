0a5781f9e770ab3197acd67eeb3a090bba8ebaba
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-18 19:48:54 +0530

    new commit

---
+++ selectors/querySelector/index.js
@@ -0,0 +1,19 @@
+
+const para = document.querySelector('p')
+
+console.log(para);      // <p> tag
+
+console.log(para[0]);      // undefined
+
+
+
+const class1 = document.querySelector('.para')
+
+console.log(class1);    // <p class=''> tag
+
+
+
+
+const id1= document.querySelector('#para')
+
+console.log(id1);       // <p id=''> tag
