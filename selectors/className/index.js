3ae148b1f859235f8892476a99643cb68a19affa
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-18 19:46:49 +0530

    new commit

---
+++ selectors/className/index.js
@@ -0,0 +1,8 @@
+const para = document.getElementsByClassName('para')
+
+console.log(para);   // html collection [p.para]
+
+console.log(para[0]);    // <p class=''> tag
+
+console.log(para[1]);       // undefined
+ 
