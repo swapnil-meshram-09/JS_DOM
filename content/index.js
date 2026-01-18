c50922125f35fd5c68f2b496d80c8617f5a68bae
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-18 19:49:26 +0530

    new commit

---
+++ content/index.js
@@ -0,0 +1,15 @@
+
+const para = document.querySelector('p')
+console.log(para);
+
+const p1 = para.innerHTML
+console.log(p1);              // visible text <tag> hidden text
+
+const p2 = para.innerText
+console.log(p2);              // visible text
+
+const p3 = para.textContent
+console.log(p3);               // visible text hidden text
+
+
+
