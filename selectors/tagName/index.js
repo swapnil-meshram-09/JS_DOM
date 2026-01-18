22ba6d577e7a3322d7b8f241450fd43cd98442ba
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-18 19:48:40 +0530

    new commit

---
+++ selectors/tagName/index.js
@@ -0,0 +1,9 @@
+const para = document.getElementsByTagName('p')
+
+console.log(para);       // html collection [p]
+
+console.log(para[0]);       // <p> tag
+
+console.log(para[1]);       // undefined
+
+
